d56eb7eb6d6e803370501287bba7e00a5cab4bcc
GnSight <ftyghome@163.com>
2022-03-29 06:39:29 +0000

    use native escaped string parser
    Signed-off-by: GnSight <ftyghome@163.com>

--- src/utils/datparser.cpp
+++ src/utils/datparser.cpp
@@ -1,4 +1,5 @@
 #include "utils/datparser.h"
+#include "utils/escapedstr.h"
 #include "utils/semaphore.h"
 #include "boost/tokenizer.hpp"
 #include "boost/asio/thread_pool.hpp"
@@ -17,57 +18,17 @@ DATParser::DATParser(boost::filesystem::path datPath, int source, int &startStam
 
 std::vector<Row *> DATParser::parseData() {
     std::vector<Row *> ret;
-    boost::escaped_list_separator<char> separator('\\', ',', '\"');
     std::ifstream stream(datPath.c_str());
     std::cout << "datPath: " << datPath.c_str() << std::endl;
-
-    std::mutex vecProt;
+    std::string line;
     int *insPos = nullptr;
     table->getPhyPosArray(insPos);
-    semaphore sem(4);
-    auto parseLineToVec = [&](std::vector<std::string> *lines) {
-        for (const auto &line: *lines) {
-            std::vector<std::string> lineRes;
-            lineRes.resize(table->colPhy);
-            int pos = 0;
-            try {
-                Tokenizer tok(line, separator);
-                auto tokenEnd = tok.end();
-                for (Tokenizer::iterator it = tok.begin(); it != tokenEnd; it++) {
-                    lineRes[insPos[pos++]] = *it;
-                }
-            } catch (boost::wrapexcept<boost::escaped_list_error> &err) {
-                std::cout << "Escaped list error! " << line << std::endl;
-            }
-            Row *row = new Row(std::move(lineRes), source, -1);
-            std::lock_guard guard(vecProt);
-            row->stamp = stamp++;
-            ret.push_back(row);
-        }
-        sem.release();
-    };
-    auto *lines = new std::vector<std::string>;
-    std::string line;
-    boost::asio::thread_pool parserPool(4);
     while (std::getline(stream, line)) {
-        lines->reserve(5000);
-        lines->push_back(line);
-        if (lines->size() >= 5000) {
-            sem.acquire();
-            boost::asio::post(parserPool, [=] {
-                parseLineToVec(lines);
-                delete lines;
-            });
-            lines = new std::vector<std::string>;
-        }
+        std::vector<std::string> lineRes;
+        lineRes.resize(table->colPhy);
+        EscapedResolver::parseEscaped(lineRes, line);
+        Row *row = new Row(std::move(lineRes), source, stamp++);
+        ret.push_back(row);
     }
-    if (!lines->empty()) {
-        boost::asio::post(parserPool, [=] {
-            parseLineToVec(lines);
-            delete lines;
-        });
-    }
-    parserPool.join();
-
     return ret;
 }
